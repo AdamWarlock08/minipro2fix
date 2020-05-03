48bb7f5c5b121ac6821fd2817c80051c652b0083
Stve Erickson <ice000@msn.com>
2020-05-03 15:13:10 -0600

    add api function

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
-const apikey = ""
+
+
+function getdata(){
+
+    var input = $('#searchtext').empty().val()
+
+var xhr = $.get("http://api.giphy.com/v1/gifs/search?q="+input+"+&api_key=rd6ZuFkNmQZZg8VfnK9VWXrr7KAPyINi&limit=25");
+
+xhr.done(function(response) {
+     console.log("success got data", response);
+    
+     var jiffs = response.data
+    
+     for (i in jiffs)
+     {
+     $('.inner').append("<img src='"+jiffs[i].images.original.url+"'>")
+     }
+    
+    
+    });
+
+}    
